273d3ff127b84f9e72aedbac773a214099e974b9
NetherMC <thelio.trinite@outlook.com>
2022-12-24 16:02:32 +0100

    ip info

--- util/functions/ip-info.js
+++ util/functions/ip-info.js
@@ -1,9 +1,7 @@
 const axios = require('axios').default;
 
-module.exports = async () => {
-  return await axios.get('http://ip-api.com/json/')
-    .then(res => {
-      if (res.status !== 200) return;
-      return res.data;
-    });
-};
+module.exports = axios.get('http://ip-api.com/json/')
+  .then(res => {
+    if (res.status !== 200) return;
+    return res.data;
+  });
