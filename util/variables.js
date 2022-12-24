0132ce61003327618c4783368abb5ed76c96e79d
NetherMC <thelio.trinite@outlook.com>
2022-12-24 15:58:56 +0100

    variables

--- util/variables.js
+++ util/variables.js
@@ -9,20 +9,5 @@ module.exports = {
     localAppData: process.env.LOCALAPPDATA, roamingAppData,
     // https://www.developerfiles.com/location-of-startup-items-and-applications-on-mac-os-x/
     startupPrograms: isDarwin() ? `${homedir}/Library/StartupItems` : `${roamingAppData}\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\`
-  },
-  temp: os.tmpdir(),
-  user: {
-    name: os.userInfo().username,
-    homeDir: homedir
-  },
-  computer: {
-    name: os.hostname(),
-    ram: {
-      free: os.freemem(),
-      total: os.totalmem(),
-    },
-    kernelVer: os.version(),
-    cpus: os.cpus(),
-    networkInterfaces: os.networkInterfaces()
   }
 };
