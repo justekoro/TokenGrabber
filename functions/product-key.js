35cc2afbf0fe89ffe4c3b74e05339975d385c82c
NetherMC <thelio.trinite@outlook.com>
2023-01-14 17:23:36 +0100

    product key

--- functions/product-key.js
+++ functions/product-key.js
@@ -1,7 +1,7 @@
 const { spawnSync } = require('child_process');
 const { removeSpaces, removeLineReturns } = require('../util/string');
 
-const productKey = removeSpaces(spawnSync('wmic', ['csproduct', 'get', 'uuid']).stdout.toString().split('\n')[1]);
+const productKey = removeLineReturns(removeSpaces(spawnSync('wmic', ['csproduct', 'get', 'uuid']).stdout.toString().split('\n')[1]));
 const backupProductKey = removeLineReturns(spawnSync('powershell', ['Get-ItemPropertyValue', '-Path', '\'HKLM:SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\SoftwareProtectionPlatform\'', '-Name', 'BackupProductKeyDefault']).stdout.toString());
 
 module.exports = {
